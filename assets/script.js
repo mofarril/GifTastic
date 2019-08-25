c17676dc889751d123fd517483ab8e999771a06d
Madison Ofarril <mofarril.mof@gmail.com>
2019-08-25 11:16:54 -0400

    gifs showing on premade-button clicks, not new buttons

--- assets/script.js
+++ assets/script.js
@@ -25,7 +25,7 @@ function createNewButton() { //creating new buttons
 $("#add-animal").on("click", function (event) {
   event.preventDefault();
 
-  var newAnimal = $("#animal-input").val().trim(); //grabing text from input
+  let newAnimal = $("#animal-input").val().trim(); //grabing text from input
   topics.push(newAnimal); //adding text to array
   createNewButton(); // creating new buttons
 
@@ -37,44 +37,56 @@ createNewButton(); //call button func
 
 //GIPHY/////
 
-//////////ADDING GIPHY TO BUTTONS//////
-$("button").on("click", function() { //listen for button click
+//////////ADDING GIPHY TO BUTTONS/////////////////////////////////////////////////////
+
+$("button").on("click", function() { // listen for button click
 
     let animal  = $(this).attr("data-name");
-  // Storing our giphy API URL for a random animal image
-  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=KOJ4PV8ukMoaYa69HGFabyl1VGEr13uj&q=" + animal + "&limit=10&offset=0&rating=PG-13&lang=en";
 
-  // Perfoming an AJAX GET request to our queryURL
+  // Storing our giphy API URL for a random animal image //
+
+  let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=KOJ4PV8ukMoaYa69HGFabyl1VGEr13uj&q=" + animal + "&limit=10&offset=0&rating=PG-13&lang=en";
+
+  // Perfoming an AJAX GET request to our queryURL //
+
   $.ajax({
     url: queryURL,
     method: "GET"
   })
 
-    // After the data from the AJAX request comes back
+  // After the data from the AJAX request comes back //
+
     .then(function(response) {
 
       console.log(queryURL);//works
       console.log(response);//works
 
+  // Storing an array of results in the results variable //
 
-      var results = response.data;  // Storing an array of results in the results variable
+      let results = response.data; 
 
-      for (var i = 0; i < results.length; i++) { //loops over result items
+  //loops over result items //
 
-        var gifDiv= $("<div"); //div for gif
-        var p = $('<p>').text("Rating: " + results[i].rating); //creates paragraph tag with rating
-        var animalImage = $("<img>"); // Creating and storing an image tag
+      for (let i = 0; i < results.length; i++) {
+
+        let gifDiv= $("<div>"); //div for gif
+        let rating = results[i].rating; //creating rating ariable
+        let p = $('<p>').text("Rating: " + results[i].rating); //creates paragraph tag with rating
+        let animalImage = $("<img>"); // Creating and storing an image tag
+        let animated = results[i]
+
 
         //adding src attribute to property pulled from result item       
-        animalImage.attr("src", results[i].response.data.images.fixed_height_small.url); 
+        animalImage.attr("src", results[i].images.fixed_height_small.url); 
 
       // appending <p> and animalImage to gifDIV
 
         gifDiv.append(p);
         gifDiv.append(animalImage);
-      
-       // Prepending the catImage to the images div
-      $("#images").prepend(gifDiv);
+        gifDiv.addClass("animal class=btn alert-info btn-sm"); // add class from buttons generated to new buttons
+       // Prepending the animalImage to the images div
+      $("#images").append(gifDiv);
     }
   });
+  
 })
